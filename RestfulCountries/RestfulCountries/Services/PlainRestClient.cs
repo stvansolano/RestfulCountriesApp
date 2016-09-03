f5f1a91894f41171d947a7fd998bfc74afb1ca06
stvansolano <stvansolano@gmail.com>
2016-09-03 08:33:19 -0600

    Added simple HttpClient usage for app + UI

---
+++ RestfulCountries/RestfulCountries/Services/PlainRestClient.cs
@@ -0,0 +1,49 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace RestfulCountries
+{
+    public class PlainRestClient
+    {
+        private const string BASE_URL = "https://restcountries.eu/rest/v1/all";
+
+        public async Task<string> GetData()
+        {
+            using (var httpClient = new HttpClient())
+            {
+                return await httpClient.GetStringAsync(BASE_URL).ConfigureAwait(false);
+            }
+        }
+
+        public async Task<IEnumerable<Country>> GetCountries()
+        {
+            var result = Enumerable.Empty<Country>();
+
+            using (var httpClient = new HttpClient())
+            {
+                httpClient.DefaultRequestHeaders.Accept.Clear();
+                httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                var response = await httpClient.GetAsync(BASE_URL).ConfigureAwait(false);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var json = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+
+                    if (!string.IsNullOrWhiteSpace(json))
+                    {
+                        result = await Task.Run(() => {
+                            return JsonConvert.DeserializeObject<IEnumerable<Country>>(json);
+                        }).ConfigureAwait(false);
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
